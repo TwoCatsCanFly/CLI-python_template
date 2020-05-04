34f70762eec010accd681595a0d7ce9ac776e4c0
TwoCatsCanFly <TwoCatsCanFly@users.noreply.github.com>
2020-05-04 22:21:06 +0300

    Добавил защиту от кривых функций Добавил очистку консоли Фиксы и оптимизации

--- CLI-temlplate.py
+++ CLI-temlplate.py
@@ -1,29 +1,25 @@
-""" Простой шаблон для консольных программ"""
+""" Простой шаблон для консольных программ. Автор CCF"""
 import os
 
-lang_pack = {'text':{'RU':{'inp':'\nВвод: ',
-                            'q': '\nВыход из программы',
-                            'errinp': 'Некоректный ввод',
-                            'wip': '\nФункция в разработке',
-                            'settings':'Доступные параметры:',
-                            'param_name':'Введите номер параметра для изменения: ',
-                            'param_value':'Изменить параметр на ',
-                            'err_param':'Недопустимый параметр',
+lang_pack = {'text':{'RU':{'inp':'Ввод: ','q': '\nВыход из программы','errinp': 'Некоректный ввод',
+                            'wip': '\nФункция в разработке','settings':'Доступные параметры:',
+                            'param_name':'Введите номер параметра для изменения: ','err_func':'Ошибка функции',
+                            'param_value':'Изменить параметр на ','err_param':'Недопустимый параметр',
                             'available_param':'Возможные параметры',
                             'about':'Это шаблон для программ на python с консольным интерфейсом)))\nАвтор: CCF'}},
-             'menu':{'RU':{'main':{'s':'Настройки','q':'Q. Выход',},
+             'menu':{'RU':{'main':{'C':'Очистить экран','S':'Настройки','Q':'Q. Выход',},
                            'back_to_main':{'m':'Вернутся в главное меню'},
                            'example':{1:'something',2:'once again something'}}}}
 app_settings = {'1':{'language':'RU'}}
 available_settings = {'language':['RU']}
 txt_list = lang_pack['text'][app_settings['1']['language']]
 menu_txt = lang_pack['menu'][app_settings['1']['language']]
-
-
 def about():
     print('-'*80)
     print(txt_list['about'])
     print('-'*80+'\n')
+def main_menu():
+    menu(menu_txt['main'])
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 def menu(menu_lines):
@@ -33,6 +29,12 @@ def wip():
     print(txt_list['wip'])
 def wrong_input():
     print(txt_list['errinp'])
+def error_function(err):
+    print('{}: {}'.format(txt_list['err_func'],err))
+def clear_screen(inp):
+    if inp not in ['C', 'c', 'С', 'с']: return False
+    clear()
+    main_menu()
 def settings(inp):
     if inp not in ['S', 's', 'ы', 'Ы']: return False
     clear()
@@ -59,45 +61,55 @@ def settings(inp):
                     break
                 if b in ['m','M','ь','Ь']: break
                 else:
-                    print(txt_list['err_param'],)
+                    print(txt_list['err_param'])
                     continue
-
         clear()
         continue
-
-    #else: return False
 def quit_program(inp):
-    if inp in ['Q','q','й','Й']:
-        print(txt_list['q'])
-        quit()
-    else:return False
+    if inp not in ['Q','q','й','Й']: return False
+    print(txt_list['q'])
+    quit()
+
+'''
+
+
+
+ОСНОВНАЯ ЧАСТЬ ДЛЯ КОДА
+
+
+
+'''
+
 def app_main():
     while True:
         i = input(txt_list['inp'])
         if quit_program(i)!=False:continue
         elif settings(i)!=False:continue
-        #здесь вписывать свои функции
+        elif clear_screen(i)!=False:continue
+        try:#здесь вписывать свои функции
 
-        elif example_function(i)!=False:continue
+            if example_function()!=False:continue
+            elif example_function(i)!=False:continue
+            elif example_function(i)!=False:continue
+            elif example_function(i)!=False:continue
+            elif example_function(i)!=False:continue
 
-        #^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        else:
-            wrong_input()
+            else:
+                wrong_input()
+                continue
+        except Exception as err:
+            error_function(err)
             continue
 
-def example_function(input):
-    if input in ['У', 'у', 'E', 'e']: #ввод при котором функция выполняется
-        # тело
-        print('\n'+'-' * 170)
-        print('Образец функции')
-        print('\n'+'-' * 170)
-        #/тело
-    else: return False
+def example_function(inp):# эта функция обрабатывает функции меню
+    if inp not in ['У', 'у', 'E', 'e']: return False #ввод при котором функция выполняется
+    # тело
+    print('\n'+'-' * 170)
+    print('Образец функции')
+    print('-' * 170)
+    #/тело
 
-'''
-ОСНОВНАЯ ЧАСТЬ ДЛЯ КОДА
-'''
 
 about()
-menu(menu_txt['main'])
+main_menu()
 app_main()
